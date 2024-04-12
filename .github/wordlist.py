d03daaec8f0dde452b981c1461ece9fc48f3a77d
IsPowers <ipowers@macalester.edu>
2024-04-12 14:05:22 -0500

    wordslist so far

---
+++ .github/wordlist.py
@@ -0,0 +1,15 @@
+import random
+
+words = ["Huxley", "Pants", "Robert", "Martin"]
+
+def select_word(words):
+    return random.choice(words)
+
+print(select_word(words))
+
+#def print_secret_word(secret_word)
+    #print("_" * len(secret_word))
+
+#print("Welcome")
+#secret_word = select_word(words)
+#print_secret_word(secret_word)
