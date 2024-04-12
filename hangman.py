02e11cbfbdc724cd0f07e24323ec2540df4f92a0
andrewdong04 <adong@MACALESTER.EDU>
2024-04-12 12:55:22 -0500

    fixed bugs in draw_man function

--- hangman.py
+++ hangman.py
@@ -13,40 +13,54 @@ def pick_word(wordslist):
 
 def draw_man(turt, text, error_count):
     '''
-    A function that draws the different parts based on the updated error count
+    A function that draws the different parts based on the updated error count,
+    and writes how many available errors are remaining
     '''
     turt.up()
     text.up()
     text.goto(100, 0)
     text.down()
+
     for cnt in range(error_count):
         text.clear()
-        text.write(
+        error_message = 'Errors remaining', 5 - cnt
+        text.write(error_message)
+
         turt.up()
         turt.goto(-100, 0)
         turt.down()
         turt.setheading(270)
         if cnt == 0:
-            turt.goto(-150, 0)
+            turt.up()
+            turt.goto(-150, 50)
+            turt.down()
             turt.circle(50)
-        elif cnt == 0:
+        elif cnt == 1:
             turt.setheading(270)
             turt.forward(100)
         elif cnt == 2:
             turt.setheading(315)
             turt.forward(100)
         elif cnt == 3:
+            turt.setheading(225)
+            turt.forward(100)
+        elif cnt == 4:
             turt.up()
             turt.goto(-100, -100)
             turt.setheading(225)
             turt.down()
             turt.forward(100)
-        elif cnt == 4:
+        elif cnt == 5:
             turt.up()
             turt.goto(-100, -100)
             turt.setheading(315)
             turt.down()
             turt.forward(100)
+            text.clear()
+            text.write("YOU LOSE :(")
+
+def play_game():
+
 
 
 # def main():
@@ -54,7 +68,8 @@ win = turtle.Screen()
 man = turtle.Turtle()
 text = turtle.Turtle()
 
+
 man.speed(0)
-draw_man(man, text, 5)
+draw_man(man, text, 6)
 
 win.exitonclick()
